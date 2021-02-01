623b063463cab59874782b6d7d3b2be45dab5861
Stanislav-Karasyk <99.kara.99@gmail.com>
2021-02-01 17:53:28 +0200

    на проверку

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
+const btnStartRef = document.querySelector('[data-action="start"]');
+const btnStopRef = document.querySelector('[data-action="stop"]');
+let timerId = null;
 
-import styles from './css/styles.css';
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+colorSwitch = () => {
+  timerId = setInterval(() => {
+    const randomIntegerFromInterval = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    const index = randomIntegerFromInterval(0, 5);
+    document.body.style.backgroundColor = colors[index];
+    btnStartRef.setAttribute("disabled", "disabled");
+  }, 1000);
+};
+
+btnStartRef.addEventListener("click", colorSwitch);
+
+btnStopRef.addEventListener("click", () => {
+  clearInterval(timerId);
+  btnStartRef.removeAttribute("disabled");
+});
